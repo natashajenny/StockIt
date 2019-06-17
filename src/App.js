943f7e3e82a5af1aa1ca6ce91f9e66fc74bbadc4
ianwwl1 <30430914+ianwwl1@users.noreply.github.com>
2019-06-17 17:35:42 +1000

    Add files via upload
    Setup for the frontend using googles material-ui platform as a base

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+        <NavBar />
+    </div>
+  );
+}
+
+export default App;
