02a3d425b3ba2b0f6e34f0bb567bb0f303106c18
Alina Sari <a.sari@unsw.edu.au>
2019-06-21 10:34:18 +1000

    Fixed inconsistency, added react router, added drawer and index files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
-import NavBar from './components/NavBar';
+import React from 'react';
+import { Route, Router } from 'react-router-dom';
+
+import history from './history';
+import { Portfolio, Metrics, AboutUs, Profile, Settings, Tutorial, Watchlist } from './pages';
 import './App.css';
 
-
-function App() {
+function App () {
   return (
     <div>
-        <NavBar />
+      <Router history={history}>
+        <Route exact path='/' component={Portfolio} />
+        <Route path='/Portfolio' component={Portfolio} />
+        <Route path='/Metrics' component={Metrics} />
+        <Route path='/AboutUs' component={AboutUs} />
+        <Route path='/Profile' component={Profile} />
+        <Route path='/Settings' component={Settings} />
+        <Route path='/Tutorial' component={Tutorial} />
+        <Route path='/Watchlist' component={Watchlist} />
+      </Router>    
     </div>
-  );
+  );  
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export * from './AboutUs';
+export * from './Metrics';
+export * from './Portfolio';
+export * from './Profile';
+export * from './Settings';
+export * from './Tutorial';
+export * from './Watchlist';
