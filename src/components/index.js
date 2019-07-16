c9f747e2aa40f5f04bb1ba7387864532629dc42a
ianwwl1 <ianwwl@gmail.com>
2019-07-16 23:13:51 +1000

    removed class gridlist not in use

--- src/components/index.js
+++ src/components/index.js
@@ -5,8 +5,5 @@ export * from './PerformanceChart';
 export * from './forms';
 export * from './WatchItems';
 export * from './WatchGrid';
-<<<<<<< HEAD
 export * from './NewsGrid';
-=======
 export * from './SearchBar';
->>>>>>> origin/alina-fe
--- src/pages/Tutorial.jsx
+++ src/pages/Tutorial.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import {newsGrid} from '../components/NewsGrid/NewsGrid';
 
 import { styles } from './styles';
 
@@ -9,6 +10,7 @@ export class PureTutorial extends React.Component {
         return (
             <div className = {classes.root}>
                 <h1> Tutorial </h1>
+                {newsGrid()}
             </div>
         );  
     }
