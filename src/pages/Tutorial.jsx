dfe9d582a7ba1e22c7e0035e7742419195b12525
ianwwl1 <ianwwl@gmail.com>
2019-07-30 21:53:41 +1000

    updated tutorial need slava to explain the prediction model further

--- src/pages/Tutorial.jsx
+++ src/pages/Tutorial.jsx
@@ -1,32 +1,16 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-// import {newsGrid} from '../components/NewsGrid/NewsGrid';
-// import {Stocklist} from '../components/Stocklist/Stocklist';
-// import {StockDrawer} from '../components/StockDrawer/StockDrawer';
 import { styles } from './styles';
-import {AddStocks } from '../components/AddStocks/AddStocks';
-// import APIClient from '../api/apiClient.js';
+// import {AddStocks } from '../components/AddStocks/AddStocks';
+import  tut1 from '../static/images/tut1.png';
+import  tut2 from '../static/images/tut2.png';
+import  tut3 from '../static/images/tut3.png';
+import  tut4 from '../static/images/tut4.png';
+import  tut5 from '../static/images/tut5.png';
 
 
  class PureTutorial extends React.Component {
 
-    // constructor(props){
-    //     super(props);
-    //     this.state = {
-    //       items: [],
-    //       isLoaded: false,
-    //     }
-    //   }
-    // componentDidMount(){
-    //     this.apiClient = new APIClient();
-    //     this.apiClient.getStocks()
-    //         .then(json =>{
-    //             this.setState({
-    //                 isLoaded: true,
-    //                 items: json.stocks,
-    //             })
-    //         });
-    // }
 
     render() {
         // var { isLoaded, items } = this.state;
@@ -37,11 +21,27 @@ import {AddStocks } from '../components/AddStocks/AddStocks';
 
         return (
        
-            <div >
+            <div text-align='center' >
                 <h1> Tutorial </h1>
+                <h1> How do we predict stock prices? </h1>
+                    <h2 >1. Calculate Technical Indicators </h2>
+                    <p>write some stuff</p>
+                        <img src={tut1} alt='technical indicators'/>
+                    <h2 >2. Fourier Transforming Data </h2>
+                    <p>write some stuff</p>
+                        <img src={tut2} alt='ftd'/>
+                    <h2 >3. Comparing price to transformed data </h2>
+                    <p>write some stuff</p>
+                        <img src={tut3} alt='compare'/>
+                    <h2 >4. Correalation Heatmap plot </h2>
+                    <p>write some stuff</p>
+                        <img src={tut4} alt='heatmap'/>
+                    <h2 >5. Predicted Data vs Stock Performance </h2>
+                    <p>write some stuff</p>
+                        <img src={tut5} alt='heatmap'/>
                 {/* <Stocklist  items={items}/> */}
                 {/* <StockDrawer /> */}
-                <AddStocks />
+                {/* <AddStocks />    */}
             </div>
         );  
     // }
--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,4 +1,4 @@
-import { opacity, createStyles } from "@material-ui/core/styles";
+import {  createStyles } from "@material-ui/core/styles";
 
 export const styles = createStyles(theme => ({
   root: {
