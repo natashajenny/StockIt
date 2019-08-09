fba444d463eeb2564a9c9c8303460fc2b0faeda5
YaroslavAkimov <31073271+YaroslavAkimov@users.noreply.github.com>
2019-08-09 12:11:02 +1000

    Prediction table moved to a separate entity

--- src/backend/model.py
+++ src/backend/model.py
@@ -267,6 +267,12 @@ class StockLog(Model, Base):
     rank = Column('rank', Integer)
     company = relationship('Company', backref='stock_logs')
 
+class Prediction(Model, Base):
+    __tablename__ = 'predictions'
+    date = Column('date', Date, primary_key=True)
+    code = Column('code', String(3), ForeignKey("companies.code"), primary_key=True)
+    opening = Column('prediction', Float)    
+    
 class TestTable(Model, Base):
     __tablename__ = 'test_table'
     date = Column('date', Date, primary_key=True)
