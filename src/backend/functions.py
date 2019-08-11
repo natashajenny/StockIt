b4edd18acbd89ac23f923621e84ca08604c1ad62
Natasha Jenny <natashajenny06@gmail.com>
2019-08-11 19:19:22 +1000

    Update functions.py
    Clean up code

--- src/backend/functions.py
+++ src/backend/functions.py
@@ -5,7 +5,6 @@ from sqlalchemy.sql import exists
 
 ## User
 
-# Create and save a new user
 def create_user(login, password, name, email, phone):
     user = User(login=login, password=password, name=name, email=email, phone=phone, balance=0)
     user.save()
@@ -18,13 +17,10 @@ def delete_user(user_id):
 def find_user(user_id):
     return User().query().get(user_id)
 
-# Returns a list of this user's portfolio objects
 def get_portfolios(user_id):
     portfolios = Portfolio().query().filter(Portfolio.user_id==user_id)
     return portfolios.all()
 
-
-# check if user is registered, allows them to login
 def validate_login(login, password):
     db = Db.instance()
     user = db.session.query(User).filter(and_(User.login == login, User.password == password)).all()
@@ -33,6 +29,7 @@ def validate_login(login, password):
     else:
         return user[0]
 
+    
 
 ## Company
 
@@ -44,12 +41,10 @@ def get_stock_logs(code):
     company = get_company(code)
     return company.stock_logs
 
-# Get all companies in the db
 def get_companies():
     companies = Company().query().order_by(Company.name)
     return companies.all()
 
-# Get all companies names
 def get_com_name():
     companies = Company().query().with_entities(Company.code).order_by(Company.code)
     return companies.all()
@@ -65,12 +60,14 @@ def get_last_ticks(stocks):
     return q.all()
 
 
+
 ## Stock Log
 
 def get_stock_details(code):
     q = StockLog().query().filter(and_(StockLog.code == code, StockLog.opening.isnot(None))).order_by(desc(StockLog.date)).first()
-    print(q.__dict__)
     return q        
+
+
 
 ## Performance Log
 
@@ -83,7 +80,6 @@ def get_summary():
 
 def get_pl_details(code):
     q = PerformanceLog().query().filter(PerformanceLog.code == code).order_by(desc(PerformanceLog.year)).first()
-#     print(q.__dict__)
     return q
 
 def get_top_ten():
@@ -93,6 +89,8 @@ def get_top_ten():
 def get_bottom_ten():
     q = StockLog().query().filter(StockLog.rank.isnot(None)).order_by(desc(StockLog.rank)).limit(10)
     return q.all()
+
+
 
 ## Portfolio
 
@@ -111,10 +109,7 @@ def delete_portfolio(user_id, portfolio_id):
 def find_portfolio(portfolio_id):
     return Portfolio().query().get(portfolio_id)
 
-# def update_portfolio(portfolio_id, net_gain):
-#     p = Portfolio().query().filter(Portfolio.portfolio_id == portfolio_id).scalar()
-#     p.net_gain = net_gain
-#     p.update()
+
 
 ## Portfolio Log
 
@@ -123,27 +118,19 @@ def get_logs(portfolio_id):
     subq = db.session.query(StockLog.code.label('stock_code'), func.max(StockLog.date).filter(StockLog.opening.isnot(None)).label('recent_date')).group_by(StockLog.code).subquery('t2')
     q = db.session.query(StockLog).join(subq, and_(StockLog.date == subq.c.recent_date, StockLog.code == subq.c.stock_code)).\
         join(PortfolioLog, PortfolioLog.code == StockLog.code).filter(PortfolioLog.portfolio_id == portfolio_id)
-    for p in q.all():
-        print(p.__dict__)
     return q.all()
 
 def get_portfolio_stocks(portfolio_id):
     q = PortfolioLog().query().filter(PortfolioLog.portfolio_id == portfolio_id)
-    for l in q.all():
-       print(l.code)
     return q.all()
 
 def get_portfolio_codes(portfolio_id):
     q = PortfolioLog().query().filter(PortfolioLog.portfolio_id == portfolio_id)
     companies = []
-    for l in q.all():
-       companies.append(l.code)
     return companies
 
 def get_log_date(portfolio_id, code):
     d = PortfolioLog().query().filter(and_(PortfolioLog.portfolio_id == portfolio_id, PortfolioLog.code == code)).scalar()
-#     for l in d.all():
-#         print(l.__dict__)
     return d.datetime
 
 def get_logs_limit(portfolio_id, start_date, end_date):
@@ -154,7 +141,6 @@ def get_bought_price(portfolio_id, code):
     log = PortfolioLog().query().filter(and_(PortfolioLog.portfolio_id == portfolio_id, PortfolioLog.code == code)).scalar()
     return log.bought_price
 
-# only save 1 log to portfolio
 def save_log(portfolio_id, code, number, bought_price):
     check = PortfolioLog().query().filter(and_(PortfolioLog.portfolio_id == portfolio_id, PortfolioLog.code == code)).scalar()
     if check is None:
@@ -181,15 +167,6 @@ def get_quantity(portfolio_id, code):
     d = PortfolioLog().query().filter(and_(PortfolioLog.portfolio_id == portfolio_id, PortfolioLog.code == code)).scalar()
     return d.number
 
-## News Log
-
-def add_news(date, code, news, score):
-    log = NewsLog(date=date, code=code, news=news, score=score)
-    log.save()
-
-def get_news(code):
-    news = NewsLog().query().filter(code==code)
-    return news.all()
 
 
 ## Watchlist
@@ -201,13 +178,11 @@ def create_wl(user_id, code):
 
 def get_wl(user_id):
     wl = Watchlist().query().filter(Watchlist.user_id==user_id)
-#     for l in wl.all():
-#         print(l.__dict__)
     return wl.all()
 
-# def get_wl_stock(user_id, code):
-#     wl = Watchlist().query().filter(and_(Watchlist.user_id==user_id, Watchlist.code==code)).scalar()
-#     return wl
+def get_wl_stock(user_id, code):
+    wl = Watchlist().query().filter(and_(Watchlist.user_id==user_id, Watchlist.code==code)).scalar()
+    return wl
 
 def delete_wl(user_id, code):
     p = Watchlist().query().filter(and_(Watchlist.user_id == user_id, Watchlist.code == code)).scalar()
@@ -235,18 +210,15 @@ def update_alerts(user_id, code, alert_high, alert_low, buy_high, buy_low, sell_
     wl.update()
     
 
+    
 ## Functions for testing
 
 def get_all_users():
     users = User().query()
-    for l in users.all():
-        print(l.__dict__)
     return users.all()
 
 def get_all_portfolios():
     p = Portfolio().query()
-    for l in p.all():
-        print(l.__dict__)
     return p.all()
 
 def get_all_pl():
